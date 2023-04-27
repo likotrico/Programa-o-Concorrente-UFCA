57384ef1446899e98560cf7b5049e33d9122ed65
likotrico <112947493+likotrico@users.noreply.github.com>
2023-04-27 13:58:28 -0300

    Update PCExercicio9.c

--- PCExercicio9.c
+++ PCExercicio9.c
@@ -4,33 +4,39 @@
 
 
 int main(){
-
+    float a, b, x, d;
     float result;
     omp_set_num_threads(3);
     #pragma omp parallel
     {
         #pragma omp single
         {
-            float a, b, result, x, d;
             #pragma omp task depend(out: x)
-                x = 1;
-                printf("%f\n", x);
+                {
+                    x = 1;
+                    printf("x: %.1f\n", x);
+                }
             #pragma omp task depend(out: d)
-                d = -15;
-            #pragma omp task depend(out: a)
-                a = x * x * x;
+                {
+                    d = -15;
+                    printf("d: %.1f\n", d); 
+                }
+            #pragma omp task depend (in: x) depend(out: a) 
+                {
+                    a = x * x * x;
+                    printf("a: %.1f\n", a); 
+                }
             #pragma omp task depend(in: x) depend(out: b)
-                b = 3*x*x;
+                {
+                    b = 3*x*x;
+                    printf("b: %.1f\n", b);
+                }
             #pragma omp task depend(in: a) depend(in: b) depend(in: d) depend(out: result)
-                result = a + b + d;
-                printf("%f\n", a);            
-                printf("%f\n", b); 
-                printf("%f\n", d); 
+                {
+                    result = a + b + d;
+                    printf("result: %.1f\n", result);
+                }
         }
     }
-    
-    
-      
-
     return 0;
 }
